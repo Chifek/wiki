4bb83f31a61fd6acb56dd1580506c16510179a20
chifek <chifek@gmail.com>
2020-12-20 20:52:06 +0300

    Auto commit

--- commit.sh
+++ commit.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+Year=`date +%Y`
+Month=`date +%m`
+Day=`date +%d`
+Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo `date` \
 
 git add . \
 
-git commit -m "testing.." \
+git commit -m "Auto commit " \
 
 git push origin master

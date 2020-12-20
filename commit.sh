1433e5fee5d864ad69b76b57c78547a317690b3e
chifek <chifek@gmail.com>
2020-12-20 22:55:47 +0300

    Auto commit in 20-12-2020 -- 22-55-47

--- commit.sh
+++ commit.sh
@@ -9,5 +9,5 @@ Minute=`date +%M`
 Second=`date +%S`
 
 
-cd /var/www/html/wiki.chifek.ru/ && git add . && git commit -m "Auto commit in $Day-$Month-$Year -- $Hour-$Minute-$Second" && git psuh origin master
+cd /var/www/html/wiki.chifek.ru/ && git add . && git commit -m "Auto commit in $Day-$Month-$Year -- $Hour-$Minute-$Second" && git push origin master
 
